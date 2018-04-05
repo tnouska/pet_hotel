14a388861616dfa50049f403823574b4518c76ce
Teagan Nouska <tnouska@live.com>
2018-04-05 16:07:29 -0500

    GET routes working need to update SQL queries

--- server/routes/dashboard.router.js
+++ server/routes/dashboard.router.js
@@ -1,7 +1,32 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
-
+router.get('/', (req, res) => {
+    let ownerId = req.body.id;
+    if (ownerId !== undefined) {
+        const queryText = `SELECT * FROM owner WHERE id = $1;`;
+        pool.query(queryText, [ownerId])
+            .then((response) => {
+                const responseArray = response.rows;
+                res.send(responseArray);
+            })
+            .catch((err) => {
+                console.log('error in dashboard.router.get: ', err);
+                res.sendStatus(500);
+            });
+    } else {
+        const queryText = `SELECT * FROM owner;`;
+        pool.query(queryText)
+            .then((response) => {
+                const responseArray = response.rows;
+                res.send(responseArray);
+            })
+            .catch((err) => {
+                console.log('error in dashboard.router.get: ', err);
+                res.sendStatus(500);
+            });
+    }
+});
 
 module.exports = router;
 //end of file
--- server/routes/home.router.js
+++ server/routes/home.router.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
-
+router.get('/',(req,res)=>{
+    let queryText = "SELECT * FROM pets WHERE checked_in = 'true' ORDER BY id ASC";
+    pool.query(queryText).then((result) => {
+        const responseArray = result.rows;
+        // console.log('inside of GET responseArray: ', responseArray);
+        res.send(responseArray);
+    });//end pool.query for getting all rental properties
+});//end router.get listener
 
 module.exports = router;
 //end of file
